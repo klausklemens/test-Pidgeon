25fbee1cec9ee436e82ffd3b12296adf7f812d16
klausklemens <vincent.regener@iserv-gis.de>
2017-04-09 08:31:01 +0200

    Added Sample-Files for Html and Div-TextField test

---
+++ Html/Div-TextField/index.js
@@ -0,0 +1,10 @@
+﻿function load() {
+  var textAreas = document.getElementsByClassName('textarea');
+  for (i=0;i<textAreas.length;i++) {
+    textAreas[i].contentEditable = true;
+  }
+}
+
+function scan() {
+  //var field = document.getElementsByClassName('textarea')[0];
+}
